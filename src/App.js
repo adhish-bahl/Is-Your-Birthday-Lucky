2358002c8b7d0fb18d7c705968c45c736e55b4aa
Adhish Bahl <70909675+adhish-bahl@users.noreply.github.com>
2021-07-22 21:54:21 +0530

    fixed footer logos

--- src/App.js
+++ src/App.js
@@ -1,4 +1,8 @@
 import "./styles.css";
+import githubLogo from "../src/github.svg";
+import linkedinLogo from "../src/linkedin.svg";
+import twitterLogo from "../src/twitter.svg";
+import portfolioLogo from "../src/portfolio.svg";
 
 export default function App() {
   return (
@@ -6,7 +10,7 @@ export default function App() {
       <div className="container">
         <section className="nitro">
           <h1>Is Your Birthday Lucky?</h1>
-          <a href="#mainSection">Scroll down to check!</a>
+          <a href="#mainSection">Click to scroll down to check!</a>
         </section>
         <section id="mainSection">
           <p className="privacyNotice">
@@ -24,7 +28,7 @@ export default function App() {
               </label>
             </p>
             <p>
-              <input type="date" id="date" name="date" />
+              <input required type="date" id="date" name="date" />
             </p>
             <p>
               <label htmlFor="number">
@@ -32,7 +36,7 @@ export default function App() {
               </label>
             </p>
             <p>
-              <input type="number" id="number" name="number" />
+              <input required type="number" id="number" name="number" />
             </p>
             <p>
               <button type="button" className="btn">
@@ -44,22 +48,22 @@ export default function App() {
             <ul>
               <li className="footerLink">
                 <a href="https://github.com/adhish-bahl">
-                  <i className="fab fa-github" aria-hidden="true"></i>
+                  <img src={githubLogo} alt="github" />
                 </a>
               </li>
               <li className="footerLink">
                 <a href="https://twitter.com/adhish_bahl">
-                  <i className="fab fa-twitter"></i>
+                  <img src={twitterLogo} alt="twitter"></img>
                 </a>
               </li>
               <li className="footerLink">
                 <a href="https://www.linkedin.com/in/adhish-bahl-4113591b7/">
-                  <i className="fab fa-linkedin-in"></i>
+                  <img src={linkedinLogo} alt="linkedin"></img>
                 </a>
               </li>
               <li className="footerLink">
                 <a href="http://adhish-bahl.netlify.app/">
-                  <i className="fas fa-briefcase"></i>
+                  <img src={portfolioLogo} alt="portfolio"></img>
                 </a>
               </li>
             </ul>
