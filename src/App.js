2a664d45e6d581b158f223166b7cb1b389471ef4
Adhish Bahl <70909675+adhish-bahl@users.noreply.github.com>
2021-07-22 20:37:15 +0530

    basic jsx done

--- src/App.js
+++ src/App.js
@@ -8,6 +8,70 @@ export default function App() {
           <h1>Is Your Birthday Lucky?</h1>
           <a href="#check">Scroll down to check!</a>
         </section>
+        <section id="mainSection">
+          <p className="privacyNotice">
+            <h2>Privacy Notice!</h2>
+            We are not storing your Data
+            <span role="img" aria-labelledby="crossIcon" className="crossIcon">
+              &#10060;
+            </span>
+          </p>
+          <h1>Enter Your Birthdate and Lucky Number below to continue</h1>
+          <form>
+            <p>
+              <label htmlFor="date">Birth Date</label>
+            </p>
+            <p>
+              <input type="date" id="date" name="date" />
+            </p>
+            <p>
+              <label htmlFor="number">Lucky Number</label>
+            </p>
+            <p>
+              <input type="number" id="number" name="number" />
+            </p>
+            <p>
+              <button type="button">Check</button>
+            </p>
+          </form>
+          <footer>
+            <ul>
+              <li className="footerLink">
+                <a href="https://github.com/adhish-bahl">
+                  <i className="fab fa-github" aria-hidden="true"></i>
+                </a>
+              </li>
+              <li className="footerLink">
+                <a href="https://twitter.com/adhish_bahl">
+                  <i className="fab fa-twitter"></i>
+                </a>
+              </li>
+              <li className="footerLink">
+                <a href="https://www.linkedin.com/in/adhish-bahl-4113591b7/">
+                  <i className="fab fa-linkedin-in"></i>
+                </a>
+              </li>
+              <li className="footerLink">
+                <a href="http://adhish-bahl.netlify.app/">
+                  <i className="fas fa-briefcase"></i>
+                </a>
+              </li>
+            </ul>
+
+            <div className="legacyText">
+              ©2021 | Adhish Bahl |{" "}
+              <a
+                href="#alertBox"
+                onClick={() => {
+                  setDisplayAlert("flex");
+                }}
+                style={{ cursor: "pointer", color: "Black" }}
+              >
+                Privacy Policy
+              </a>
+            </div>
+          </footer>
+        </section>
       </div>
     </div>
   );
